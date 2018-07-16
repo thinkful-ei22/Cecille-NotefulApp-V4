6477c8490edc32eb54b5ec84eae13d9da9e142e3
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-16 13:20:00 -0700

    Required express, mongoose, passport, User model, and created and exported router in routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const User = require('../models/user')
+
+const router = express.Router();
+
 const options = {session: false, failWithError: true};
 
 const localAuth = passport.authenticate('local', options);
@@ -5,3 +12,5 @@ const localAuth = passport.authenticate('local', options);
 router.post('/login', localAuth, function (req, res) {
   return res.json(req.user);
 });
+
+module.exports = router;
