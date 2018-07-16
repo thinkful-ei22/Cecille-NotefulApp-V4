5971353719c3522a1a4f0f92444f96f0aa273646
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-16 12:38:42 -0700

    Edited routes/users.js to s combine firstname and lastname to fullname and pulled the req.body variables

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,17 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
-const { User } = require('./models/user');
+const User = require('../models/user');
 
 const router = express.Router();
 
 router.post('/users', (req, res) => {
-  return User
+  const { fullname, username, password } = req.body;
+  User
     .create({
+      fullname,
       username,
-      password,
-      firstName,
-      lastName
+      password
     })
     .then(user => {
       return res.status(201).location(`/api/users/${user.id}`).json(user.serialize());
