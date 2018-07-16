95f742d70193499f11e553bfa0ec17791d008f75
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-16 11:53:32 -0700

    Required User model in models/users.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,10 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const { User } = require('./models/user');
 
 const router = express.Router();
+
+
 
 module.exports = router;
