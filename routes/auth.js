51fe5cae24daac9ef38fd1952724afdc60d3fb1c
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-17 08:38:43 -0700

    Edited login endpoint in auth.js to return a token instead of the user

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,8 @@ const options = {session: false, failWithError: true};
 const localAuth = passport.authenticate('local', options);
 
 router.post('/login', localAuth, function (req, res) {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user);
+  return res.json({ authToken });
 });
 
 module.exports = router;
