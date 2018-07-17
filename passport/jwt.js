8db92082914e5450afbd550a4430b636472bd50c
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-17 09:14:47 -0700

    Created jwtStrategy with options and exported for use in server.js

--- passport/jwt.js
+++ passport/jwt.js
@@ -1,2 +1,14 @@
 const { Strategy: jwtStrategy, ExtractJwt } = require('passport-jwt');
 const { JWT_SECRET } = require('../config');
+
+const options = {
+  secretOrKey: JWT_SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+  algorithms: ['HS256']
+};
+
+const jwtStrategy = new jwtStrategy(options, (payload, done) => {
+  done(null, payload.user);
+})
+
+module.exports = jwtStrategy;
