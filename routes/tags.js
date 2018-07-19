ddf3ab4d6be8bbf4753fcae32306ef0560602301
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-19 13:04:35 -0700

    Added userId to routes/notes.js - got missed from yesterday

--- routes/tags.js
+++ routes/tags.js
@@ -13,8 +13,8 @@ router.use('/', passport.authenticate('jwt', { session: false, failWithError: tr
 
 /* ========== GET/READ ALL ITEMS ========== */
 router.get('/', (req, res, next) => {
-
-  Tag.find()
+const userId = req.user.id;
+  Tag.find({ userId })
     .sort('name')
     .then(results => {
       res.json(results);
