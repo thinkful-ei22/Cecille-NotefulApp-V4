af8dcf632ee4644a8f577ed588a98f832c476c2f
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-20 06:43:41 -0700

    Created handlers-notes.js and implemented initial setup

---
+++ handlers/handlers-notes.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Note = require('../models/note');
+const Folder = require('../models/folder');
+const Tag = require('../models/tags');
+const { getAllNotesHandler, getNoteByIdHandler, postNoteHandler, putNoteHandler, deleteNoteHandler } = require('../handlers/handlers-notes');
+
+const getAllNotesHandler = (req, res, next) => {
+
+}
+
+const getNoteByIdHandler = (req, res, next) => {
+
+}
+
+const postNoteHandler = (req, res, next) => {
+
+}
+
+const putNoteHandler = (req, res, next) => {
+
+}
+
+const deleteNoteHandler = (req, res, next) => {
+
+}
+
+module.exports = { getAllNotesHandler, getNoteByIdHandler, postNoteHandler, putNoteHandler, deleteNoteHandler};
