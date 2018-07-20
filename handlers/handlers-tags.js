1fa453d0e314341ddc3cda41c615977b2ef950a1
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-20 06:32:22 -0700

    Created handlers-tags.js and implemented initial setup

---
+++ handlers/handlers-tags.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Tag = require('../models/tag');
+const Note = require('../models/note');
+
+const getAllTagsHandler = (req, res, next) => {
+
+}
+
+const getTagByIdHandler = (req, res, next) => {
+
+}
+
+const postTagHandler = (req, res, next) => {
+
+}
+
+const putTagHandler = (req, res, next) => {
+
+}
+
+const deleteTagHandler = (req, res, next) => {
+  
+}
+
+module.exports = { getAllTagsHandler, getTagByIdHandler, postTagHandler, putTagHandler, deleteTagHandler};
